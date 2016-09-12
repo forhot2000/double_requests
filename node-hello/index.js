a57da8f8b7c2f217b9206e7839c4b6e956b3df57
West Wang <west@fossil.com>
2016-09-12 19:30:50 +0800

    init

---
+++ node-hello/index.js
@@ -0,0 +1,13 @@
+var koa = require('koa');
+var app = module.exports = koa();
+
+app.use(function* () {
+  yield new Promise((resolve, reject) => {
+    setTimeout(() => {
+      this.body = 'Hello asynchronous world!';
+      resolve();
+    }, 1000);
+  });
+});
+
+if (!module.parent) app.listen(3000);
